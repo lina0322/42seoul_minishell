e6bbcf815f84e628f573289972a0ce8f88f0231a
lina0322 <llim@student.42seoul.kr>
2021-03-12 23:36:14 +0900

    Feat #5 - 환경변수 파싱하는 parse_evn 구현

---
+++ parse_env.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_env.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llim <llim@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/12 22:38:22 by llim              #+#    #+#             */
+/*   Updated: 2021/03/12 23:35:51 by llim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void parse_env(char **envp, t_state *state)
+{
+	int		i;
+	int		j;
+	char	*key;
+	char	*value;
+
+	i = 0;
+	while (envp[i])
+	{
+		j = 0;
+		while (envp[i][j])
+		{
+			if (envp[i][j] == '=')
+			{
+				key = ft_substr(envp[i], 0, j);
+				value = ft_substr(envp[i], j + 1, ft_strlen(envp[i]) - j - 1);
+				break;
+			}
+			j++;
+		}
+		add_back_env(&(state->env_head);
+		i++;
+	}
+}
+
