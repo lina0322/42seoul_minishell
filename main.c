52125cf0e0eab566f3ac40409edb8ce55db2702d
lina0322 <llim@student.42seoul.kr>
2021-03-13 00:12:51 +0900

    Feat - state init하는 함수 구현

--- main.c
+++ main.c
@@ -6,19 +6,26 @@
 /*   By: dhyeon <dhyeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 17:26:45 by dhyeon            #+#    #+#             */
-/*   Updated: 2021/03/11 17:29:26 by dhyeon           ###   ########.fr       */
+/*   Updated: 2021/03/13 00:11:54 by llim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int argc, char **argv, char *envp[])
+int		main(int argc, char **argv, char *envp[])
 {
 	t_state	state;
 
-	// init_state(&state);
+	init_state(&state);
 	parse_env(envp, &state);
 	// signal
-	prompt(&state);
+	// prompt(&state);
 	// tokenizer(&state);
+}
+
+void	init_state(t_state *state)
+{
+	state->token_head = 0;
+	state->env_head = 0;
+	state->input = 0;
 }
