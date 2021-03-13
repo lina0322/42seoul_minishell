883ab07f8e9af6262c23ceb28488317c04e66210
Lina.L <llim@student.42seoul.kr>
2021-03-13 16:34:21 +0900

    Add #5 - export 관련 함수 분리하여 파일 생성

---
+++ export.c
@@ -0,0 +1,89 @@
+void	print_export_sorted_list(t_env *env)
+{
+	char	**env_list;
+	int		len;
+	int		i;
+
+	len = check_env_length(env);
+	env_list = (char **)malloc(sizeof(char *) * len);
+	if (env_list == NULL)
+		return ;
+	i = 0;
+	while (env)
+	{
+		env_list[i] = make_export_env(env->key, env->value);
+		env = env->next;
+		i++;
+	}
+	sorted_list(env_list, len);
+	i = 0;
+	while (env_list[i])
+	{
+		printf("declare -x %s\n", env_list[i]);
+		i++;
+	}
+}
+
+int		check_env_length(t_env *env)
+{
+	int	len;
+
+	len = 0;
+	while (env)
+	{
+		len++;
+		env = env->next;
+	}
+	return (len);
+}
+
+char 	*make_export_env(char *key, char *value)
+{
+	char	*result;
+	int		len;
+	int		i;
+	int		j;
+
+	len = ft_strlen(key) + ft_strlen(value) + 4;
+	result = (char *)malloc(sizeof(char) * len);
+	if (result == NULL)
+		return (0);
+	i = 0;
+	j = 0;
+	while (key[i])
+		result[j++] = key[i++];
+	result[j++] = '=';
+	result[j++] = '\"';
+	i = 0;
+	while (value[i])
+		result[j++] = value[i++];
+	result[j++] = '\"';
+	result[j] = '\0';
+	return (result);
+}
+
+void	sorted_list(char **list, int size)
+{
+	int		i;
+	int		j;
+	int		n;
+	char	*temp;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		j = 0;
+		while (j < size - 1 - i)
+		{
+			if (ft_strncmp(list[j], list[j+1], ft_strlen(list[j])) > 0)
+			{
+			//	printf("%s, %s\n", list[j], list[j+1]);
+				temp = list[j];
+				list[j] = list[j + 1];
+				list[j + 1] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
