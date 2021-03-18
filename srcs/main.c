6f50496e434675734b08cb199294a8c84e229cee
lina0322 <llim@student.42seoul.kr>
2021-03-18 19:51:08 +0900

    Refactor #4 - main.c 정리

--- srcs/main.c
+++ srcs/main.c
@@ -6,23 +6,11 @@
 /*   By: dhyeon <dhyeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 17:26:45 by dhyeon            #+#    #+#             */
-/*   Updated: 2021/03/16 01:09:35 by dhyeon           ###   ########.fr       */
+/*   Updated: 2021/03/18 19:50:41 by llim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void	handle_signal(int signo)
-{
-	if (signo == SIGINT)
-	{
-		printf("\b\b \nbash> ");
-	}
-	else if (signo == SIGQUIT)
-	{
-		printf("\b\b");
-	}
-}
 
 int	main(int argc, char **argv, char *envp[])
 {
@@ -30,11 +18,6 @@ int	main(int argc, char **argv, char *envp[])
 
 	(void)argc;
 	(void)argv;
-	(void)envp;
-	// if (argc > 10)
-	// 	return 0;
-//	if (argv[0][1] == '1')
-//		return 0;
 	init_state(&state);
 	signal(SIGINT, (void *)handle_signal);
 	signal(SIGQUIT, (void *)handle_signal);
@@ -46,13 +29,6 @@ int	main(int argc, char **argv, char *envp[])
 		free(state.input);
 		// tokenizer(&state);
 	}
-
-	//test
-//	print_env_all(state.env_head);
-//	print_export(state.env_head);
-	// print_one_export(state.env_head, argv[1]);
-	// update_env(state.env_head, argv[1], argv[2]);
-	// print_one_export(state.env_head, argv[1]);
 }
 
 void	init_state(t_state *state)
