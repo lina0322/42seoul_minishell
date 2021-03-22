94a7804bacfe67f8c28879e87efcf9478c8e65b3
Dasom Hyeon <dhyeon@c1r3s3.42seoul.kr>
2021-03-22 20:26:56 +0900

    Refactor #8 - echo 인자 n 기능 추가

--- srcs/ft_echo.c
+++ srcs/ft_echo.c
@@ -6,11 +6,36 @@
 /*   By: dhyeon <dhyeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/22 14:42:21 by dhyeon            #+#    #+#             */
-/*   Updated: 2021/03/22 15:48:53 by dhyeon           ###   ########.fr       */
+/*   Updated: 2021/03/22 20:21:10 by dhyeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	check_echo_flag(char **av, int *i, int *flag)
+{
+	int	j;
+
+	*i = 1;
+	if (av[1] && av[*i][0] == '-' && av[*i][1] == 'n')
+	{
+		while (av[*i][0] == '-' && av[*i][1] == 'n')
+		{
+			j = 1;
+			while (av[1][j] != '\0')
+			{
+				if (av[1][j] != 'n')
+				{
+					// (*i)--;
+					return ;
+				}
+				j++;
+			}
+			(*i)++;
+		}
+		*flag = 1;
+	}
+}
 
 void    ft_echo(t_state *t, t_cmd *cmd)
 {
@@ -19,21 +44,24 @@ void    ft_echo(t_state *t, t_cmd *cmd)
 	(void)cmd;
 	(void)t;
 	//test
-	char	*av[] = {"echo", "-n", "Hello", "World", 0};
+	// char	*av[] = {"echo", "-n", "-n", "-n", "Hello", "World", 0};
+	// int		ac = 6;
+	// //test
+	// char	*av[] = {"echo", "Hello", "World", 0};
+	// int		ac = 3;
+	// test
+	char	*av[] = {"echo", "-nnnnnn", "Hello", "World", 0};
 	int		ac = 4;
 
 	flag = 0;
-	i = -1;
-	if (!ft_strncmp(av[1], "-n\0", 3))
+	i = 0;
+	check_echo_flag(av, &i, &flag);
+	while (i < ac)
 	{
-		flag = 1;
-		i++;
-	}
-	while (++i < ac - 1)
-	{
-		write(1, av[i + 1], ft_strlen(av[i + 1]));
-		if (i < ac - 2)
+		write(1, av[i], ft_strlen(av[i]));
+		if (i < ac - 1)
 			write(1, " ", 1);
+		i++;
 	}
 	if (flag == 0)
 		write(1, "\n", 1);
