171a70e756b6aece1f90b69055a06f0a22a4cc15
jachoi <jachoi@student.42seoul.kr>
2021-03-26 21:26:02 +0900

    Refactor #8 - export 함수 수정

--- srcs/ft_export.c
+++ srcs/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: dhyeon <dhyeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/21 13:55:05 by dhyeon            #+#    #+#             */
-/*   Updated: 2021/03/24 15:53:27 by dhyeon           ###   ########.fr       */
+/*   Updated: 2021/03/26 21:22:09 by dhyeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,17 @@ int	parse_export(t_env *tmp, char *str)
 	while (str[klen] != '=')
 		klen++;
 	if (!ft_calloc(sizeof(char *), klen + 1, (void *)& tmp->key))
-		return (0);
-	// tmp->key = (char *)malloc(sizeof(char) * (klen + 1));
-	while (str[klen + 1 + vlen] != '\0')
-		vlen++;
-	if (!ft_calloc(sizeof(char *), vlen + 1, (void *)& tmp->value))
-		return (0);
-	// tmp->value = (char *)malloc(sizeof(char) * (vlen + 1));
+		return (0); // 말록실패는 exit처리
 	i = -1;
 	while (++i < klen)
 		tmp->key[i] = str[i];
 	tmp->key[i] = '\0';
+	if (str[klen + 1] == '\0')
+		return (0);
+	while (str[klen + 1 + vlen] != '\0')
+		vlen++;
+	if (!ft_calloc(sizeof(char *), vlen + 1, (void *)& tmp->value))
+		return (0);
 	i = -1;
 	while (++i < vlen)
 		tmp->value[i] = str[i + klen + 1];
@@ -41,35 +41,31 @@ int	parse_export(t_env *tmp, char *str)
 	return (1);
 }
 
-void	ft_export(t_state *state, t_cmd *cmd)
+void	ft_export(t_state *s, t_cmd *cmd)
 {
 	(void)cmd;
-	// //test 1
-	// int ac = 4;
-	// char *av[] = {"export", "TEST", "=", "123", "0"};
+	int	i;
 	//test 2
 	int ac = 2;
 	char *av[] = {"export", "TEST2=456", "0"};
 	t_env tmp;
 
 	if (ac == 1) // cmd->ac
-		print_export(state->env_head);
+		print_export(s->env_head);
 	else
 	{
-		if (ac == 2) // cmd->ac
+		i = 1;
+		while (i < ac)
 		{
-			// tmp.key = 0;
-			// tmp.value = 0;
-			parse_export(&tmp, av[1]); // = 없는것 체크해야함
-			add_env_back(&state->env_head, tmp.key, tmp.value, 1); // cmd->av
+			if (ft_strrchr(av[i], '=') == 0) // = 이 없는경우 (1. key값만 있는경우)
+				add_env_back(&s->env_head, av[i], 0, 0);
+			else // = 가 있는 경우
+			{
+				if (parse_export(&tmp, av[i]) == 0) // value 값이 없는 경우 value = 0 으로
+					tmp.value = 0;
+				add_env_back(&s->env_head, tmp.key, tmp.value, 1);
+			}
+			i++;
 		}
-		// else if (ac == 3)
-		// {
-		// 	//나중에 추가 -> has_equal = 0
-		// }
-		// else
-		// {
-		// 	add_env_back(&state->env_head, av[1], av[3], 1); // cmd->av
-		// }
 	}
 }
--- srcs/ft_pwd.c
+++ srcs/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: dhyeon <dhyeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/15 23:52:39 by dhyeon            #+#    #+#             */
-/*   Updated: 2021/03/16 02:22:01 by dhyeon           ###   ########.fr       */
+/*   Updated: 2021/03/26 21:24:45 by dhyeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,16 +32,16 @@ int	builtin(t_state *state, t_cmd *cmd)
 
 	if (!ft_strncmp(state->input, "pwd", 3)) // 나중에 input 대신 cmd의 명령어로 수정해야함
 		ft_pwd(state, cmd);
-	// else if (!ft_strncmp(state->input, "echo", 4))
-		// ft_echo(state, cmd);
+	else if (!ft_strncmp(state->input, "echo", 4))
+		ft_echo(state, cmd);
 	else if (!ft_strncmp(state->input, "cd", 2))
 		ft_cd(state, cmd);
 	else if (!ft_strncmp(state->input, "env", 3))
 		print_env_all(state->env_head);
-	// else if (!ft_strncmp(state->input, "export", 6))
-	// 	ft_export(state, cmd);
-	// else if (!ft_strncmp(state->input, "unset", 5))
-	// 	ft_unset(state, cmd);
+	else if (!ft_strncmp(state->input, "export", 6))
+		ft_export(state, cmd);
+	else if (!ft_strncmp(state->input, "unset", 5))
+		ft_unset(state, cmd);
 	else if (!ft_strncmp(state->input, "exit", 4))
 		ft_exit(state, cmd);
 	return (0);
