b5494367f582fbd17e55779e331e0481295ed9d2
Dasom Hyeon <dhyeon@c2r3s1.42seoul.kr>
2021-03-30 19:45:47 +0900

    Style - builtin 함수 이동

--- srcs/ft_pwd.c
+++ srcs/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: dhyeon <dhyeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/15 23:52:39 by dhyeon            #+#    #+#             */
-/*   Updated: 2021/03/30 19:37:03 by dhyeon           ###   ########.fr       */
+/*   Updated: 2021/03/30 19:45:02 by dhyeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,29 +22,5 @@ int	ft_pwd(t_state *s, t_cmd *cmd)
 		return (-1); //에러처리
 	else
 		printf("%s\n", buf);
-	return (0);
-}
-
-int	builtin(t_state *s, t_cmd *cmd)
-{
-	//test용
-	(void)cmd;
-
-	if (!ft_strcmp(s->input, "pwd")) // 나중에 input 대신 cmd의 명령어로 수정해야함
-		ft_pwd(s, cmd);
-	else if (!ft_strcmp(s->input, "echo"))
-		ft_echo(s, cmd);
-	else if (!ft_strcmp(s->input, "cd"))
-		ft_cd(s, cmd);
-	else if (!ft_strcmp(s->input, "env"))
-		print_env_all(s->env_head);
-	else if (!ft_strcmp(s->input, "export"))
-		ft_export(s, cmd);
-	else if (!ft_strcmp(s->input, "unset"))
-		ft_unset(s, cmd);
-	else if (!ft_strcmp(s->input, "exit"))
-		ft_exit(s, cmd);
-	else if (!ft_strcmp(s->input, "test"))
-		find_command(s, cmd);
 	return (0);
 }
