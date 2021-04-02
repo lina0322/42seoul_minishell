4fd190aa44f3a8f50236755f0133e70899d375a6
lina0322 <llim@student.42seoul.kr>
2021-04-03 04:01:19 +0900

    Feat #11 - 띄어쓰기 관련 추가 구현

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dhyeon <dhyeon@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: llim <llim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 17:26:45 by dhyeon            #+#    #+#             */
-/*   Updated: 2021/04/03 01:02:14 by dhyeon           ###   ########.fr       */
+/*   Updated: 2021/04/03 03:57:43 by llim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,11 @@ int		main(int argc, char **argv, char *envp[])
 	parse_path(&state);
 	while (1)
 	{
-	 	prompt(&state);
-	// 	builtin(&state, state.cmd_head); // 임시파일
-	 	tokenizer(&state);
-		state.token_head = 0;
-	// 	parse_cmd(&state);
+		prompt(&state);
+	 	builtin(&state, state.cmd_head); // 임시파일
+		tokenizer(&state);
+		ft_memset((void *)&state.token_head, 0, sizeof(t_token));
+		ft_memset((void *)&state.cmd_head, 0, sizeof(t_cmd));
 	}
 }
 
