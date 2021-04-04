22820552818f12cb92825864d8a82f0690c1e367
lina0322 <llim@student.42seoul.kr>
2021-04-04 21:35:37 +0900

    Feat #3 - cmd 프리하고, token과 cmd memset하는 부분 분리

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dhyeon <dhyeon@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: llim <llim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 17:26:45 by dhyeon            #+#    #+#             */
-/*   Updated: 2021/04/04 14:23:30 by dhyeon           ###   ########.fr       */
+/*   Updated: 2021/04/04 21:35:02 by llim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,12 +28,17 @@ int		main(int argc, char **argv, char *envp[])
 		prompt(&state);
 	 	builtin(&state, state.cmd_head); // 임시파일
 		tokenizer(&state);
-		free_cmd(state.cmd_head);
-		ft_memset((void *)state.token_head, 0, sizeof(t_token));
-		state.token_head = 0;
- 		ft_memset((void *)state.cmd_head, 0, sizeof(t_cmd));
-		state.cmd_head = 0;
+		prepare_token_and_cmd(&state);
 	}
+}
+
+void	prepare_token_and_cmd(t_state *state)
+{
+	free_cmd(state->cmd_head);
+	ft_memset((void *)state->token_head, 0, sizeof(t_token));
+	state->token_head = 0;
+ 	ft_memset((void *)state->cmd_head, 0, sizeof(t_cmd));
+	state->cmd_head = 0;
 }
 
 void	init_state(t_state *state)
