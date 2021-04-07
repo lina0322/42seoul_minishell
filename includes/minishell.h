b8d09fda09f20e6de8dc1176a0f531d9613cdd16
lina0322 <llim@student.42seoul.kr>
2021-04-08 02:27:20 +0900

    Refactor #2 - env와 backslash 확인하는 함수 분리

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: llim <llim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 17:29:42 by dhyeon            #+#    #+#             */
-/*   Updated: 2021/04/07 19:16:04 by llim             ###   ########.fr       */
+/*   Updated: 2021/04/07 23:51:20 by llim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -208,7 +208,8 @@ int		builtin(t_state *s, t_cmd *cmd);
 */
 void	parse_cmd(t_state *state);
 void	make_cmd(t_state *state, t_token *start, int ac, int type);
-void	check_env_and_backslash(t_state *state, t_token *token);
+void	check_env(t_state *state, t_token *token);
+void	check_backslash(t_token *token);
 void	add_cmd_back(t_cmd **head, char **av, int type);
 t_cmd	*create_cmd(char **av, int ac, int type);
 void	free_cmd(t_cmd *cmd);
