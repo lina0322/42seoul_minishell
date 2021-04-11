7209845410090caa771a9f95b106a43e8c918cc4
llim <llim@student.42seoul.kr>
2021-04-11 17:21:34 +0900

    Refactor #11 - norm때문에 함수 분리

--- srcs/cmd_parse.c
+++ srcs/cmd_parse.c
@@ -12,6 +12,30 @@
 
 #include "minishell.h"
 
+///for test
+void	print_cmd(t_state *state)
+{
+	t_cmd	*cmd = state->cmd_head;
+	int		i = 0;
+	char string[2];
+	string[1] = 0;
+	
+	while (cmd)
+	{
+		string[0] = '0' + cmd->type;
+		tputs(string, 0, ft_putchar);
+		while (i < cmd->ac)
+		{
+			tputs(cmd->av[i], 0, ft_putchar);
+			tputs(",", 0, ft_putchar);
+			
+			i++;
+		}
+		cmd = cmd->next;
+		tputs("\n", 0, ft_putchar);
+	}
+}
+
 void	parse_cmd(t_state *state)
 {
 	t_token	*token;
@@ -26,29 +50,22 @@ void	parse_cmd(t_state *state)
 	while (token)
 	{
 		ac++;
-		if (token->type == ERROR_QUOTE)
+		if (token->type >= PIPE || !token->next)
 		{
-			make_cmd(state, start, ac, ERROR_QUOTE);
-			break;
-		}
-		else if (token->type == PIPE || token->type == SEMICOLON || !token->next)
-		{
-			if (token->type == SPACE || token->type == PIPE || token->type == SEMICOLON)
+			if (token->type == SPACE || token->type >= PIPE)
 				ac--;
 			make_cmd(state, start, ac, type);
-			if (token->type == PIPE)
-				type = PIPE_TYPE;
-			else
-				type = COLON_TYPE;
+			if (token->type >= PIPE)
+				type = token->type;
 			ac = 0;
 			start = token->next;
-		} 
+		}
 		else if (token->type == SPACE)
 			ac--;
-		if (token)
-			token = token->next;
+		token = token->next;
 	}
 	free_token(state->token_head);
+	print_cmd(state);
 }
 
 void	make_cmd(t_state *state, t_token *start, int ac, int type)
@@ -56,8 +73,8 @@ void	make_cmd(t_state *state, t_token *start, int ac, int type)
 	char	**av;
 	int		i;
 
-    if (!ft_calloc(ac + 1, sizeof(char *), (void **)& av))
-	    return ;
+	if (!ft_calloc(ac + 1, sizeof(char *), (void **)& av))
+		return ;
 	i = ac + 1;
 	while (i > 0)
 		av[--i] = 0;
@@ -71,79 +88,12 @@ void	make_cmd(t_state *state, t_token *start, int ac, int type)
 		else
 		{
 			if (start->type == DOUBLE || start->type == COMMON)
-			{
-				check_backslash(start);
-				check_env(state, start);
-			}
+				check_backslash_and_env(state, start);
 			av[i] = ft_strjoin2(av[i], start->str);
 		}
 		start = start->next;
 	}
 	add_cmd_back(&state->cmd_head, av, type);
-}
-
-void	check_backslash(t_token *token)
-{
-	int i;
-	char *tmp;
-	char *tmp2;
-	
-	i = 0;
-	while (token->str[i])
-	{
-		if (token->str[i] == '\\' && 
-		(token->str[i + 1] == '\\' || token->str[i + 1] == '\"'))
-		{
-			tmp = ft_substr(token->str, 0, i);
-			tmp2 = ft_strjoin2(tmp, &token->str[i + 1]);
-			free(token->str);
-			token->str = ft_strdup(tmp2);
-			free(tmp);
-			free(tmp2);
-		}
-		i++;
-	}
-}
-
-void	check_env(t_state *state, t_token *token)
-{
-	int i;
-	int j;
-	char *value;
-	char *temp;
-	char *temp2;
-
-	i = 0;
-	while (token->str[i])
-	{
-		if (token->str[i] == '$' && token->str[i + 1])
-		{
-			j = i + 1;
-			while (token->str[j])
-			{
-				if (token->str[j] == '\'' || token->str[j] == ' ')
-					break;
-				j++;
-			}
-			temp = ft_substr(token->str, i + 1, j - 1 - i); // key
-			if (!ft_strcmp(temp, "?"))
-			{
-				i++;
-				continue;
-			}
-			value = ft_strdup(find_env_val(state->env_head, temp));
-			free(temp);
-			temp = ft_substr(token->str, 0, i); // $이전 str
-			temp2 = ft_strjoin2(temp, value); // $이전 str에 value 합침
-			temp = ft_substr(token->str, j, ft_strlen(token->str)); // 환경변수 이후 str
-			free(token->str);
-			token->str = ft_strjoin2(temp2, temp); // $이전 str에 value합친거 뒤에 환경변수 이후 str까지 더함
-			free(value);
-			free(temp2);
-			free(temp);
-		}
-		i++;
-	}
 }
 
 void	add_cmd_back(t_cmd **head, char **av, int type)
@@ -170,9 +120,9 @@ t_cmd	*create_cmd(char **av, int ac, int type, t_cmd *prev)
 	t_cmd *cmd;
 
 	if (av == NULL)
-		return NULL;
-    if (!ft_calloc(1, sizeof(t_cmd), (void *)& cmd))
-	    return (NULL);
+		return (NULL);
+	if (!ft_calloc(1, sizeof(t_cmd), (void *)& cmd))
+		return (NULL);
 	cmd->av = av;
 	cmd->ac = ac;
 	cmd->type = type;
