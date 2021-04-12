296aa9d850c07e51fdc7897f6f516a95aea599e2
som-syom <noenna@naver.com>
2021-04-13 07:53:20 +0900

    Feat #6 - ft_exit 추가해야할 부분 수정

--- srcs/ft_exit.c
+++ srcs/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: dhyeon <dhyeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 01:39:54 by dhyeon            #+#    #+#             */
-/*   Updated: 2021/03/16 01:41:13 by dhyeon           ###   ########.fr       */
+/*   Updated: 2021/04/13 07:52:50 by dhyeon           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	ft_exit(t_state *state, t_cmd *cmd)
 {
 	(void)state;
 	(void)cmd;
+	// exit 매개변수 확인하여 인자로 넘기기 추가
 	// All Free 함수 구현후 추가
 	printf("exit\n");
 	exit(0);
