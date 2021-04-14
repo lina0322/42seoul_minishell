1fbd925693c168e6c141980f467bb3304f57f187
Dasom Hyeon <dhyeon@c2r6s5.42seoul.kr>
2021-04-14 17:25:51 +0900

    Feat #6 - exit 인자 있는 경우 추가, $? 값 전달

--- srcs/ft_exit.c
+++ srcs/ft_exit.c
@@ -6,18 +6,57 @@
 /*   By: dhyeon <dhyeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 01:39:54 by dhyeon            #+#    #+#             */
-/*   Updated: 2021/04/13 07:52:50 by dhyeon           ###   ########seoul.kr  */
+/*   Updated: 2021/04/14 17:08:09 by dhyeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_exit(t_state *state, t_cmd *cmd)
+int	is_num_str(char *str)
 {
-	(void)state;
-	(void)cmd;
-	// exit 매개변수 확인하여 인자로 넘기기 추가
+	int	i;
+
+	i = 0;
+	if (str[0] == '-' || str[0] == '+')
+		i++;
+	while (str[i])
+	{
+		if (!('0' <= str[i] && str[i] <= '9'))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	exit_not_num(t_cmd *cmd)
+{
+	printf("bash: exit: %s: numeric argument required",  cmd->av[1]);
+	exit(255);
+}
+
+void	ft_exit(t_state *state, t_cmd *cmd)
+{
+	int ret;
+	
 	// All Free 함수 구현후 추가
-	printf("exit\n");
-	exit(0);
+	if (cmd->ac == 1)
+	{
+		printf("exit\n");
+		exit(state->ret);
+	}
+	else if (cmd->ac == 2)
+	{
+		if (is_num_str(cmd->av[1]))
+		{
+			ret = ft_atoi(cmd->av[1]);
+			printf("%d\n", ret);
+			exit(ret);
+		}
+		else
+			exit_not_num(cmd);
+	}
+	else
+	{
+		printf("bash: exit: too many arguments\n");
+	}
 }
