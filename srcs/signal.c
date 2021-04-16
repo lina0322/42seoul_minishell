6da4cfe98715c8b19c5bf9460bfefbf867bade2f
som-syom <noenna@naver.com>
2021-04-17 06:05:54 +0900

    Feat #7 - signal처리

--- srcs/signal.c
+++ srcs/signal.c
@@ -6,7 +6,7 @@
 /*   By: dhyeon <dhyeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 19:51:53 by llim              #+#    #+#             */
-/*   Updated: 2021/04/17 05:56:59 by dhyeon           ###   ########seoul.kr  */
+/*   Updated: 2021/04/17 06:05:20 by dhyeon           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,16 @@ void	handle_signal(int signo)
 			g_state.input = 0;
 		}
 		else
+		{
+			g_state.ret = 130;
 			write(1, "\n", 1);
+		}
 	}
 	else if (signo == SIGQUIT)
 	{
 		if (g_state.is_fork == 1)
 		{
+			g_state.ret = 131;
 			write(1, "Quit: 3\n", 9);
 		}
 	}
