7f08333a636ab17f20634cb9ea78922bcc20f7eb
som-syom <noenna@naver.com>
2021-04-17 03:50:34 +0900

    Style - main 정리

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: dhyeon <dhyeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 17:26:45 by dhyeon            #+#    #+#             */
-/*   Updated: 2021/04/15 01:51:04 by dhyeon           ###   ########seoul.kr  */
+/*   Updated: 2021/04/17 03:50:16 by dhyeon           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ int		main(int argc, char **argv, char *envp[])
 	signal(SIGINT, (void *)handle_signal);
 	signal(SIGQUIT, (void *)handle_signal);
 	parse_env(envp, &state);
-	// parse_path(&state);
 	while (1)
 	{
 		prompt2(&state);
@@ -48,19 +47,4 @@ void	init_state(t_state *state)
 {
 	ft_memset((void *)state, 0, sizeof(t_state));
 	tcgetattr(0, &state->t.save);
-}
-
-void	free_2d(char **array)
-{
-	char	**temp;
-	int		i;
-
-	temp = array;
-	i = 0;
-	while (temp[i])
-	{
-		free(temp[i]);
-		i++;
-	}
-	free(temp);
 }
